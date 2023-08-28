2d726d1624307e4802c3c3370fbb04426c177693
[Keval Patel] <[khp90611@gmail.com]>
2023-08-28 00:38:37 -0400

    Created Employee DB and tables with foreign keys. Created schemas file. Need to add entries and start work on inquirer

---
+++ sql-queries/schemas.sql
@@ -0,0 +1,32 @@
+-- Create Tables--
+DROP TABLE IF EXISTS department;
+CREATE table department(
+	id INT PRIMARY KEY auto_increment,
+    name VARCHAR(30) NOT NULL
+
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role(
+	id INT PRIMARY KEY auto_increment,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS employee
+CREATE TABLE employee (
+	id int primary key auto_increment,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id int NOT NULL,
+    manager_id int,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE cascade
+);
+--------------------------
+
